f467bb419362795b7ee33267062523f0f7b917dc
numb <469395669@qq.com>
2022-04-17 10:19:40 +0800

    修改后端ocean-visualization的访问地址

--- vue.config.js
+++ vue.config.js
@@ -1,42 +1,43 @@
 'use strict'
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 // const path=require('path')
-module.exports = 
-defineConfig(
-  {
-  transpileDependencies: true,
-  lintOnSave:false,
-  devServer: {
-    port: 8000,     // 端口
-    proxy: {
-      '/vector': {
-          // target: 'https://www.baidu.com/',//后端接口地址
-          // target: 'http://localhost:8089/',
-          target: 'http://kartozageoserver:8080/',
-          changeOrigin: true,//是否允许跨越
-          ws:true,
-          pathRewrite: {
-              '^/vector': '/',//重写,
-          }
-      },
-      '/api':{
-        target: 'http://localhost:8080/',
-        changeOrigin: true,//是否允许跨越
-        ws:true,
-        pathRewrite: {
-            '^/api': '/',//重写,
+module.exports =
+    defineConfig(
+        {
+            transpileDependencies: true,
+            lintOnSave: false,
+            devServer: {
+                port: 8000,     // 端口
+                proxy: {
+                    '/vector': {
+                        // target: 'https://www.baidu.com/',//后端接口地址
+                        // target: 'http://localhost:8089/',
+                        target: 'http://kartozageoserver:8080/',
+                        changeOrigin: true,//是否允许跨越
+                        ws: true,
+                        pathRewrite: {
+                            '^/vector': '/',//重写,
+                        }
+                    },
+                    '/api': {
+                        // target: 'http://localhost:8080/',
+                        target: 'http://ocean-visualization:8080/',
+                        changeOrigin: true,//是否允许跨越
+                        ws: true,
+                        pathRewrite: {
+                            '^/api': '/',//重写,
+                        }
+                    },
+                    '/test': {
+                        target: 'https://www.baidu.com',
+                        changeOrigin: true,//是否允许跨越
+                        ws: true,
+                        pathRewrite: {
+                            '^/test': '/',//重写,
+                        }
+                    }
+                },
+            },
+
         }
-      },
-      '/test':{
-        target: 'https://www.baidu.com',
-        changeOrigin: true,//是否允许跨越
-        ws:true,
-        pathRewrite: {
-            '^/test': '/',//重写,
-        }
-      }
-  },
-  },
-  
-}
-)
+    )
